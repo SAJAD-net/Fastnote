27e04de8ab69d124e9e741c1222022c5115e4027
SAJADnet <sajadsajad.sch@gmail.com>
2024-03-31 07:39:48 -0400

    update fnote.sh

--- fnote.sh
+++ fnote.sh
@@ -2,10 +2,12 @@
 
 home=$HOME/.fastnote
 
+
 back() {
 	echo -n "- press enter to return ~ "
 	read
 }
+
 
 #reload the shell
 _reload() {
@@ -21,61 +23,76 @@ _reload() {
 _help() {
 	echo -e "\nusage : bash fnote.sh"
 	echo -e "args  :\n\t-i : install\n\t-h : displays this help\n"
-	back	
+	back
 }
+
 
 #builds the '.fastnote' folder in $HOME, makes an alias for fastnote
 initial() {
-	if [ -d "$home" ];then
-		echo "- fastnote is already installed !"
+	if [ -d "$home" ]
+	then
+		echo "[+] fastnote is already installed !"
+
 	else
 		mkdir $home
 		mkdir $home/notes
-		cp fastnote.sh $home/
-		if [ "$SHELL" == "bin/bash" ];then 
+		echo "# This file holds the default text editor for fastnote" >> $home/fnote_text_editor.conf
+		echo "# To use your favorite text editor, change the following line." >> $home/fnote_text_editor.conf
+		echo "default_text_editor=vi" >> $home/fnote_text_editor.conf
+		cp fnote.sh $home/
+
+		if [ "$SHELL" == "bin/bash" ]
+		then
 			echo -e "\nalias fnote=\"bash $home/fnote.sh\"" >> $HOME/.bashrc
 		else
 			echo -e "\nalias fnote=\"bash $home/fnote.sh\"" >> $HOME/.zshrc
 		fi
-		
-		echo "- fastnote initialized !"
+
+		echo "- the default text editor is set to 'vi'."
+		echo "- you can use your favorite text editor by changing fnote_text_editor.conf file."
+		echo "- fnote_text_editor.conf is in $home/"
+		echo "[+] fastnote is successfully initialized !"
 		_reload
-		echo "- now just type 'fnote' to run the fastnote tool :)"
+		echo "[+] now, just type 'fnote' to run the fastnote tool :)"
 	fi
 }
 
-#opens a file with 'ntitle' name in 'vim'
+
+#opens a file with 'ntitle' name in 'text_editor'
 new() {
-	echo -n "- enter note title name : "
+	echo -n "- note title : "
 	read ntitle
-	vim $home/notes/$ntitle
+	$text_editor $home/notes/$ntitle
 	back
 }
+
 
 #opens the 'ntitle' file if exists
 open() {
-	echo -n "- enter note title name : "
+	echo -n "- note title : "
 	read ntitle
 	if [ -f "$home/notes/$ntitle" ];then
-		vim $home/notes/$ntitle
+		$text_editor $home/notes/$ntitle
 	else
-		echo "sorry, the note with this -> '$ntitle' title doesn't exists !"
+		echo "[!] '$ntitle' note doesn't exists !"
 	fi
 	back
 }
 
+
 #deletes 'ntitle' note
 delete() {
-	echo -n "- enter note title name : "
+	echo -n "- note title : "
 	read ntitle
 	if [ -f "$home/notes/$ntitle" ];then
 		rm $home/notes/$ntitle
-		echo "- '$ntitle' successfully removed !"
+		echo "[+] '$ntitle' successfully removed !"
 	else
-		echo "sorry, the note with this -> '$ntitle' title doesn't exists !"
+		echo "[!] '$ntitle' note doesn't exists !"
 	fi
 	back
 }
+
 
 #returns a list of all the notes
 notes() {
@@ -83,66 +100,80 @@ notes() {
 	back
 }
 
+
 #to rename a note
 rename() {
-	echo -n "- enter note title name : "
+	echo -n "- note title : "
 	read ntitle
 	if [ -f "$home/notes/$ntitle" ];then
-		echo -n "- now enter new title : "
+		echo -n "- new title : "
 		read rtitle
 		mv $home/notes/$ntitle $home/notes/$rtitle
-		echo "- '$ntitle' successfully renamed to '$rtitle' !"	
+		echo "[+] '$ntitle' is successfully renamed to '$rtitle' !"
 	else
-		echo "sorry, the note with this -> '$ntitle' title doesn't exists !"
+		echo "[!] '$ntitle' note doesn't exist !"
 	fi
 	back
 }
 
-fastnote() {
-	if [ -d $home ];then
-		while [ 0 ]; do 
-			clear
-			echo -e "\t[ FAST NOTE ]"
-			echo -e "   to exit just press enter\n\
-			don't kid yourself :)\n"
-			echo -e "[1]- new\t[2]- append"
-			echo -e "[3]- read\t[4]- remove"
-			echo -e "[5]- notes\t[6]- rename"
-			echo -e "[7]- install\t[8]- help\n"
 
-			echo -n "fastnote ~ "    
-			read ope
-			
-			if [ "$ope" == "" ];then
-				break	
-			elif [ "$ope" == "1" ];then
-				new
-			elif [ "$ope" == "2" ];then
-				open
-			elif [ "$ope" == "3" ];then
-				open
-			elif [ "$ope" == "4" ];then
-				delete
-			elif [ "$ope" == "5" ];then
-				notes
-			elif [ "$ope" == "6" ];then
-				rename
-			elif [ "$ope" == "7" ];then
-				initial	
-				back
-			elif [ "$ope" == "8" ];then
-				_help
-			fi
-		done
-	else
-		echo "first install it : ./fnote.sh -i"
-	fi
+fastnote() {
+	while [ 0 ]; do
+		clear
+		echo -e "\t[ FAST NOTE ]"
+		echo -e "   to exit just press enter\n"
+		echo -e "[1]- new\t[2]- append"
+		echo -e "[3]- read\t[4]- remove"
+		echo -e "[5]- notes\t[6]- rename"
+		echo -e "[7]- install\t[8]- help\n"
+
+		echo -n "fastnote ~ "
+		read ope
+
+		if [ "$ope" == "" ];then
+			break
+		elif [ "$ope" == "1" ];then
+			new
+		elif [ "$ope" == "2" ];then
+			open
+		elif [ "$ope" == "3" ];then
+			open
+		elif [ "$ope" == "4" ];then
+			delete
+		elif [ "$ope" == "5" ];then
+			notes
+		elif [ "$ope" == "6" ];then
+			rename
+		elif [ "$ope" == "7" ];then
+			initial
+			back
+		elif [ "$ope" == "8" ];then
+			_help
+		fi
+	done
 }
 
-if [ "$1" == "-i" ];then
-       initial
-elif [ "$1" == "-h" ];then
-       _help	
-else       
-	fastnote
+
+if [ "$1" == "-i" ]
+then
+	initial
+elif [ "$1" == "-h" ]
+then
+	_help
+else
+	if [ -d $home ]
+	then
+		if [ -f "$home/fnote_text_editor.conf" ]
+		then
+			ltn=$(awk "/default_text_editor/{ print NR; exit }" $home/fnote_text_editor.conf)
+			echo $ltn
+			text_editor=$(cat $home/fnote_text_editor.conf | sed "${ltn}q;d" | sed -r "s/\w*\=//")
+			fastnote
+		else
+			echo "[!] $home/fnote_text_editor.conf not found !"
+			echo "- run the fastnote with -i falg to fix the problem"
+		fi
+	else
+		echo "- first install fastnote : ./fnote -i"
+	fi
 fi
